a617b2a9d28a8edfd7a94af396aa8bb5ded92324
Georgi Sabev <georgethebeatle@gmail.com>
2021-08-06 15:26:56 +0000

    Create 2 clusters shareing the same oidc config
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>
    Co-authored-by: Danail Branekov <danailster@gmail.com>

---
+++ cross-cluster-orgs/setup.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+ip=$(curl -s ipecho.net/plain)
+"$SCRIPT_DIR/gencert.sh" "$ip"
+
+passwd_hash=$(htpasswd -bnBC 10 "" password | tr -d ':\n')
+
+KIND_CONF="$(mktemp)"
+trap "rm $KIND_CONF" EXIT
+
+cat <<EOF >>"$KIND_CONF"
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  extraMounts:
+  - containerPath: /ssl
+    hostPath: $SCRIPT_DIR/ssl
+    readOnly: true
+  kubeadmConfigPatches:
+  - |
+    kind: ClusterConfiguration
+    apiServer:
+      extraVolumes:
+        - name: ssl-certs
+          hostPath: /ssl
+          mountPath: /etc/dex-ssl
+      extraArgs:
+        oidc-issuer-url: https://$ip.nip.io:32000
+        oidc-client-id: example-app
+        oidc-ca-file: /etc/dex-ssl/ca.crt
+        oidc-username-claim: email
+        oidc-username-prefix: "oidc:"
+        oidc-groups-claim: groups
+EOF
+
+# open oidc-related ports
+gcloud -q compute instances add-tags $(hostname) --zone europe-west2-a --tags=$(hostname)
+gcloud -q compute firewall-rules delete allow-oidc-$(hostname) || true
+gcloud compute firewall-rules create allow-oidc-$(hostname) --allow=tcp:5555,tcp:32000 --target-tags=$(hostname)
+
+kind create cluster --name cross-org-2 --config "$KIND_CONF"
+
+# only map port 32000 of cluster 1, which contains dex
+cat <<EOF >>"$KIND_CONF"
+  extraPortMappings:
+  - containerPort: 32000
+    hostPort: 32000
+    protocol: TCP
+EOF
+kind create cluster --name cross-org-1 --config "$KIND_CONF"
+
+kubectl create namespace dex
+kubectl create secret --namespace dex tls "dex-tls" --cert="$SCRIPT_DIR/ssl/server.crt" --key="$SCRIPT_DIR/ssl/server.key"
+
+cat <<EOF | kubectl apply -f -
+kind: ConfigMap
+apiVersion: v1
+metadata:
+  name: dex-config
+  namespace: dex
+data:
+  config.yaml: |
+    issuer: https://34.142.56.162.nip.io:32000
+    storage:
+      type: kubernetes
+      config:
+        inCluster: true
+    web:
+      https: 0.0.0.0:5556
+      tlsCert: /etc/dex/tls/tls.crt
+      tlsKey: /etc/dex/tls/tls.key
+    oauth2:
+      skipApprovalScreen: true
+
+    staticClients:
+    - id: example-app
+      redirectURIs:
+      - 'http://$ip.nip.io:5555/callback'
+      name: 'Example App'
+      secret: ZXhhbXBsZS1hcHAtc2VjcmV0
+
+    enablePasswordDB: true
+    staticPasswords:
+    - email: "alice@vcap.me"
+      # bcrypt hash of the string "password"
+      hash: $passwd_hash
+      username: "alice"
+      userID: "08a8684b-db88-4b73-90a9-3cd1661f5466"
+EOF
+
+kubectl create -f "$SCRIPT_DIR/dex.yaml"
+
+# Apply CF CRDs
+kubectl apply -k $SCRIPT_DIR/../../cf-crd-explorations/config/crd
+
+# Create RBAC roles
+kubectl apply -f $SCRIPT_DIR/../cf-roles-model/rbac.yaml
+
+# Create some sample resources
+kubectl apply --recursive -f $SCRIPT_DIR/../../cf-crd-explorations/config/samples/cf-crds
+
+echo Done
+echo In order to login run the following command:
+echo example-app --issuer https://$ip.nip.io:32000 --issuer-root-ca ./ssl/ca.crt --listen http://0.0.0.0:5555 --redirect-uri http://$ip.nip.io:5555/callback
+echo then navigate to http://$ip.nip.io:5555 to authenticate
