2f1dda720e857a4c30176014ee2a366025a71a86
Danail Branekov <danailster@gmail.com>
2021-08-26 12:51:07 +0000

    KubeFed setup script
    Co-authored-by: Mario Nitchev <marionitchev@gmail.com>

---
+++ kubefed/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+KUBEFED_DIR="$HOME/workspace/kubefed"
+
+if ! [[ -d "$KUBEFED_DIR" ]]; then
+  mkdir -p $KUBEFED_DIR
+  # the pr-fix-fix-joined-clusters branch contains a fix on fix-joined-kind-clusters.sh
+  git -C "$KUBEFED_DIR"/.. clone -b pr-fix-fix-joined-clusters git@github.com:eirini-forks/kubefed.git
+
+  pushd $KUBEFED_DIR
+  {
+    ./scripts/download-binaries.sh
+  }
+  popd
+fi
+
+kind create cluster --name kubefed-2
+kind create cluster --name kubefed-1
+
+pushd $KUBEFED_DIR
+{
+  KIND_CLUSTER_NAME=kubefed-1 make deploy.kind
+  kubefedctl join kind-kubefed-2
+  ./scripts/fix-joined-kind-clusters.sh
+}
+popd
+
+kubectl create ns federate-me
+kubefedctl federate ns federate-me
+
+kubefedctl federate --filename deployment.yaml | kubectl apply -f -
+kubectl apply -f deployment-replicaset-scheduling-pref.yaml
