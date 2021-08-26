041afa74c2e83e8792b53e0a216ca0059dfd2039
Danail Branekov <danailster@gmail.com>
2021-08-26 16:03:32 +0000

    Demonstrate federation of various object types

--- kubefed/deploy.sh
+++ kubefed/deploy.sh
@@ -2,6 +2,7 @@
 
 set -euxo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 KUBEFED_DIR="$HOME/workspace/kubefed"
 
 if ! [[ -d "$KUBEFED_DIR" ]]; then
@@ -17,7 +18,10 @@ if ! [[ -d "$KUBEFED_DIR" ]]; then
 fi
 
 kind create cluster --name kubefed-2
+kubectl apply -k $SCRIPT_DIR/../../cf-crd-explorations/config/crd
+
 kind create cluster --name kubefed-1
+kubectl apply -k $SCRIPT_DIR/../../cf-crd-explorations/config/crd
 
 pushd $KUBEFED_DIR
 {
@@ -32,5 +36,14 @@ kubefedctl federate --filename ../cf-roles-model/rbac.yaml | kubectl apply -f -
 kubectl create ns federate-me
 kubefedctl federate ns federate-me
 
+# Federated deployments
 kubefedctl federate --filename deployment.yaml | kubectl apply -f -
 kubectl apply -f deployment-replicaset-scheduling-pref.yaml
+
+# Federating role bindings
+kubectl apply -f alice-space-developer-rolebinding.yml
+kubefedctl federate rolebinding alice-space-developer -n federate-me --enable-type
+
+#federate CF App
+kubectl apply -n federate-me -f ./../../cf-crd-explorations/config/samples/cf-crds/app.yaml
+kubefedctl federate app my-app-guid -n federate-me --enable-type
