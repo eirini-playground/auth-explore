fc24785900fd30083f5f27f8dd756ec2654d5fe8
Danail Branekov <danailster@gmail.com>
2021-08-31 15:30:29 +0000

    Fix deploy.sh

--- run-cfshim/deploy.sh
+++ run-cfshim/deploy.sh
@@ -6,7 +6,6 @@ CF_CRD_EXPLORE_PATH="$SCRIPT_DIR/../../cf-crd-explorations"
 
 export CLIENT_ID="..."
 export CLIENT_SECRET="..."
-export USERNAME="..."
 
 echo "See dex-auth/README.MD to fill the values above"
 exit 1
@@ -17,4 +16,13 @@ kubectl apply --filename https://github.com/pivotal/kpack/releases/download/v0.3
 
 kubectl create namespace cf-workloads
 helm install eirini-controller https://github.com/cloudfoundry-incubator/eirini-controller/releases/download/v0.1.0/eirini-controller-0.1.0.tgz
-make -C "$CF_CRD_EXPLORE_PATH" install
+
+export REGISTRY_TAG_BASE=eirini/cf-crd-staging-spike/buildpack
+export PACKAGE_REGISTRY_TAG_BASE="eirini/cf-crd-staging-spike/packages"
+export REGISTRY_SECRET="app-registry-credentials"
+
+pushd "$CF_CRD_EXPLORE_PATH"
+{
+  make install
+}
+popd
